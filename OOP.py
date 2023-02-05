dde9a3476daee2f2f545162547458325d7f329f7
K-Alex-N <kurochkinalexei@yandex.ru>
2023-02-05 09:42:31 +0300

    up2

---
+++ OOP.py
@@ -0,0 +1,17 @@
+class Line:
+    def __init__(self, a, b, c, d):
+        self.sp = a, b
+        self.ep = c, d
+
+class Rect:
+    pass
+
+class Ellipse:
+    pass
+
+
+elements = []
+
+g1 = Line(a, b, c, d)
+g2 = Rect(a, b, c, d)
+g3 = Ellipse(a, b, c, d)
